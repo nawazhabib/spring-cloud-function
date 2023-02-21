383661765755ebfefff278c28b4ad184c30ddd80
nawazhabib <nawaz.habib@arya.digital>
2023-02-21 17:30:25 +0600

    got error

--- src/main/java/com/aws/springcloudawslambda/SpringCloudAwsLambdaApplication.java
+++ src/main/java/com/aws/springcloudawslambda/SpringCloudAwsLambdaApplication.java
@@ -3,25 +3,23 @@ package com.aws.springcloudawslambda;
 import com.aws.springcloudawslambda.entity.Student;
 import com.aws.springcloudawslambda.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.function.context.FunctionRegistration;
+import org.springframework.cloud.function.context.FunctionType;
+import org.springframework.cloud.function.context.FunctionalSpringApplication;
+import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.GenericApplicationContext;
 
 import java.util.List;
 import java.util.function.Function;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
-public class SpringCloudAwsLambdaApplication {
+public class SpringCloudAwsLambdaApplication implements ApplicationContextInitializer<GenericApplicationContext> {
 
 	@Autowired
 	private StudentRepository studentRepository;
-
-	@Bean
-	public Supplier<List<Student>> students() {
-		return ()-> studentRepository.studentList();
-	}
 
 	@Bean
 	public Function<String, List<Student>> findByName() {
@@ -29,13 +27,15 @@ public class SpringCloudAwsLambdaApplication {
 				.filter(student -> student.getName().equals(input)).collect(Collectors.toList());
 	}
 
-	@Bean
-	public MyConsumer myConsumerBean() {
-		return new MyConsumer();
+	@Override
+	public void initialize(GenericApplicationContext context) {
+		context.registerBean("findByName", FunctionRegistration.class,
+				() -> new FunctionRegistration<Function<String, List<Student>>> (findByName())
+						.type(FunctionType.from(String.class).to(List.class).getType()));
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudAwsLambdaApplication.class, args);
+		FunctionalSpringApplication.run(SpringCloudAwsLambdaApplication.class, args);
 	}
 
 }
--- src/main/java/com/aws/springcloudawslambda/handler/StudentHandler.java
+++ src/main/java/com/aws/springcloudawslambda/handler/StudentHandler.java
@@ -2,5 +2,5 @@ package com.aws.springcloudawslambda.handler;
 
 import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
 
-public class StudentHandler extends SpringBootRequestHandler<String, Object> {
-}
+//public class StudentHandler extends SpringBootRequestHandler<String, Object> {
+//}
